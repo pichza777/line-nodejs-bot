ec1e6b68d7ba34401af30f20dc4cbb92b8298f74
pichza777 <pichza777@gmail.com>
2017-06-16 00:50:33 +0700

    v0.02

--- index.js
+++ index.js
@@ -8,20 +8,23 @@ const config = {
   channelAccessToken: process.env.aAMls5K18jevEiIZZlJ1zyu5u8gLxv7FGOYcaHak5tt2Zni2NfzWs5nfapzErLNnBsK8TlaCnHJvxBg1md67eMxWaFPl9GE/sCKzFpC0mM1ai70aKau/lF+0svFNjCWq8Zv1+RMvO4eRAVeYfoEybwdB04t89/1O/w1cDnyilFU=,
   channelSecret: process.env.047a51ae1557c6a602e7a417d2e68182,
 };
-
 // create LINE SDK client
 const client = new line.Client(config);
 
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express();
+app.use(middleware(config))
+app.use(bp.json())
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise
+app.post('/webhook', (req, res) => {
+	//console.log(req);
+	Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
+    .then((result) => res.json(result))
+	.catch((err)=>{console.log(err)})
 });
 
 // event handler
@@ -31,11 +34,29 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  call_msgservice(event.source.userId,event.message.text)
+  .then((echo)=>client.replyMessage(event.replyToken, echo))
+  .then((result)=>Promise.resolve(result))
+  .catch((err)=>Promise.reject(result))
+}
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+// call message service
+const call_msgservice = (userId,msg) => {
+    console.log('call_msgservice: ' + msg);
+    return new Promise((resolve, reject) => {
+        const formdata = { user: userId, text: msg };
+        request.post(process.env.URL_BAEBYBOT_MESSAGE, { form: formdata }, function (error, response, body) {
+            if (!error) {
+                const rr = JSON.parse(body);
+                const echo = { type: 'text', text: rr['restext'] };
+                console.log('call_msgservice: success');
+                resolve(echo);
+            } else {
+                console.log('call_msgservice: fail');
+                reject(error);
+            }
+        })
+    })
 }
 
 // listen on port
