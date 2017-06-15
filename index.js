d51856504d70d484aa68073e41d209b8102a7173
pichza777 <pichza777@gmail.com>
2017-06-16 01:16:42 +0700

    ฟห

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
-'use strict'
-const Service = require('./lib/clients/v2/Service')
-const SignatureValidator = require('./lib/clients/v2/SignatureValidator')
+'use strict';
 
-module.exports = {
+const line = require('@line/bot-sdk');
+const middleware = require('@line/bot-sdk').middleware;
+const express = require('express');
+const request = require('request');
+const bp = require('body-parser');
 
-  init (options) {
-    this.client = new Service(options)
-    this.validator = new SignatureValidator(options)
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.aAMls5K18jevEiIZZlJ1zyu5u8gLxv7FGOYcaHak5tt2Zni2NfzWs5nfapzErLNnBsK8TlaCnHJvxBg1md67eMxWaFPl9GE/sCKzFpC0mM1ai70aKau/lF+0svFNjCWq8Zv1+RMvO4eRAVeYfoEybwdB04t89/1O/w1cDnyilFU=,
+  channelSecret: process.env.047a51ae1557c6a602e7a417d2e68182,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+app.use(middleware(config))
+app.use(bp.json())
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/webhook', (req, res) => {
+	//console.log(req);
+	Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+	.catch((err)=>{console.log(err)})
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
   }
 
+  const echo = { type: 'text', text: event.message.text };
+  
+  return client.replyMessage(event.replyToken, echo);
 }
+
+
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
